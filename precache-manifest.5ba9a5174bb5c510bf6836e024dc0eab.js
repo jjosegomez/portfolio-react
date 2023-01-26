b8230cb7e5ca67e9533cbb658d263f8734ff47e1
Juan Jose Gomez Medina <juanjo.gomez2000@hotmail.com>
2023-01-26 08:58:36 -0500

    Updates

---
+++ precache-manifest.5ba9a5174bb5c510bf6836e024dc0eab.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "370868f44c788c5a2113",
+    "url": "/portfolio-react/static/css/main.24abb61d.chunk.css"
+  },
+  {
+    "revision": "370868f44c788c5a2113",
+    "url": "/portfolio-react/static/js/main.5265ad8b.chunk.js"
+  },
+  {
+    "revision": "11528ddda4154ce1e32b",
+    "url": "/portfolio-react/static/js/runtime~main.72b3206a.js"
+  },
+  {
+    "revision": "b4a44ac516f4727f76d6",
+    "url": "/portfolio-react/static/css/2.5e580bed.chunk.css"
+  },
+  {
+    "revision": "b4a44ac516f4727f76d6",
+    "url": "/portfolio-react/static/js/2.5dbf6d97.chunk.js"
+  },
+  {
+    "revision": "2d4dcc5a0a8952d551f93785285d30b5",
+    "url": "/portfolio-react/static/media/university-of-south-florida-usf-vector-logo-2022.2d4dcc5a.svg"
+  },
+  {
+    "revision": "7e33c61a017b1fe0d6cb86b40678e181",
+    "url": "/portfolio-react/static/media/white-usf-logo.7e33c61a.svg"
+  },
+  {
+    "revision": "0e96436b4ca76869f716e7137cfb9248",
+    "url": "/portfolio-react/static/media/pp.0e96436b.jpeg"
+  },
+  {
+    "revision": "2adbc3e00b73d5333249d3f50a551422",
+    "url": "/portfolio-react/index.html"
+  }
+];
