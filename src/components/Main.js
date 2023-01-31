6aeed6884bb6729f3e1e7830321d46172ca6ff47
Juan Jose Gomez Medina <juanjo.gomez2000@hotmail.com>
2023-01-31 17:02:58 -0500

    hero image updated

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,18 @@
 import React, { useEffect } from 'react'
 import Aos from 'aos';
+import pp from "./assets/pp.jpeg"
 import 'aos/dist/aos.css'; // You can also use <link> for styles
 
 const Main = () => {
     return (
         <section className="hero-container">
+            <div className='hero-card'>
+                <img src={pp}></img>
+                <label className='card-text'>
+                    <h2 id='my-name'>Juan J Gomez</h2>
+                    <h4>Incoming Software Engineer</h4>
+                </label>
+            </div>
         </section>
     );
 }
