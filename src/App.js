1d5519c859bfd5db026c40cae7ea073ea6815620
Juan Jose Gomez Medina <juanjo.gomez2000@hotmail.com>
2023-08-11 10:42:08 -0400

    Clean up Deleting unnecesray files from V1

--- src/App.js
+++ src/App.js
@@ -1,36 +1,12 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar.js";
-import Hero from "./components/Hero/Hero";
-import Aboutme from "./components/Aboutme/Aboutme";
-import Skills from "./components/Skills/Skills";
-import Project from "./components/ProjectList/ProjectList";
-import Footer from "./components/Footer/Footer";
-import Contactme from "./components/Contactme/Contactme";
-import axios from 'axios';
-import { useState, useEffect } from 'react';
-import "./main.css"
+import "./App.css"
 
 const App = () => {
-    const [projects, setProjects] = useState([])
-
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await axios.get("http://localhost:5000/api/")
-                setProjects(response.data)
-            } catch (error) {
-                console.error(`Error while getting projects ${error}`)
-            }
-        }
-        fetchData()
-
-    }, [])
-
     return (
-        <>           
-            <Navbar />
-        </>
-
+        <div className="App">
+            <Navbar/>
+        </div>
     );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import "./main.css"
+import "./App.css"
 
 ReactDOM.render(
     <React.StrictMode>
