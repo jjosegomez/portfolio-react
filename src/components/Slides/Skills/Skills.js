6d6524f13e0bb30e8ded68d25f26b90fc34f34ce
Juan Jose Gomez Medina <juanjo.gomez2000@hotmail.com>
2023-08-21 20:08:13 -0400

    skill img added with modal and color changed

--- src/components/Slides/Skills/Skills.js
+++ src/components/Slides/Skills/Skills.js
@@ -7,99 +7,107 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import { useSpring, animated } from '@react-spring/web';
 
-const Fade = React.forwardRef(function Fade(props, ref) {
-  const {
-    children,
-    in: open,
-    onClick,
-    onEnter,
-    onExited,
-    ownerState,
-    ...other
-  } = props;
-  const style = useSpring({
-    from: { opacity: 0 },
-    to: { opacity: open ? 1 : 0 },
-    onStart: () => {
-      if (open && onEnter) {
-        onEnter(null, true);
-      }
-    },
-    onRest: () => {
-      if (!open && onExited) {
-        onExited(null, true);
-      }
-    },
-  });
+import reactIcon from "../../../assets/skills/React-icon.svg"
+import expressIcon from "../../../assets/skills/expressjs-icon.svg"
+import mongoIcon from "../../../assets/skills/mongodb-icon.svg"
+import nodeIcon from "../../../assets/skills/nodejs-icon.svg"
+import htmlIcon from "../../../assets/skills/html-5-svgrepo-com.svg"
+import cssIcon from "../../../assets/skills/css-3-logo-svgrepo-com.svg"
+import jsIcon from "../../../assets/skills/Unofficial_JavaScript_logo_2.svg"
+import pythonIcon from "../../../assets/skills/python-icon.svg"
+import cppIcon from "../../../assets/skills/ISO_C++_Logo.svg"
+import swiftIcon from "../../../assets/skills/swift-icon.svg"
+import gitLogo from "../../../assets/skills/git-icon-logo-svgrepo-com.svg"
 
-  return (
-    <animated.div ref={ref} style={style} {...other}>
-      {React.cloneElement(children, { onClick })}
-    </animated.div>
-  );
+import "./Skills.css"
+
+const Fade = React.forwardRef(function Fade(props, ref) {
+    const {
+        children,
+        in: open,
+        onClick,
+        onEnter,
+        onExited,
+        ownerState,
+        ...other
+    } = props;
+    const style = useSpring({
+        from: { opacity: 0 },
+        to: { opacity: open ? 1 : 0 },
+        onStart: () => {
+            if (open && onEnter) {
+                onEnter(null, true);
+            }
+        },
+        onRest: () => {
+            if (!open && onExited) {
+                onExited(null, true);
+            }
+        },
+    });
+
+    return (
+        <animated.div ref={ref} style={style} {...other}>
+            {React.cloneElement(children, { onClick })}
+        </animated.div>
+    );
 });
 
 Fade.propTypes = {
-  children: PropTypes.element.isRequired,
-  in: PropTypes.bool,
-  onClick: PropTypes.any,
-  onEnter: PropTypes.func,
-  onExited: PropTypes.func,
-  ownerState: PropTypes.any,
+    children: PropTypes.element.isRequired,
+    in: PropTypes.bool,
+    onClick: PropTypes.any,
+    onEnter: PropTypes.func,
+    onExited: PropTypes.func,
+    ownerState: PropTypes.any,
 };
 
 const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: 400,
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
-  boxShadow: 24,
-  p: 4,
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
 };
 
-function SpringModal() {
-  const [open, setOpen] = React.useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
+function SpringModal({ skill, img, description }) {
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
 
-  return (
-    <div>
-      <Button onClick={handleOpen}>Open modal</Button>
-      <Modal
-        aria-labelledby="spring-modal-title"
-        aria-describedby="spring-modal-description"
-        open={open}
-        onClose={handleClose}
-        closeAfterTransition
-        slots={{ backdrop: Backdrop }}
-        slotProps={{
-          backdrop: {
-            TransitionComponent: Fade,
-          },
-        }}
-      >
-        <Fade in={open}>
-          <Box sx={style}>
-            <Typography id="spring-modal-title" variant="h6" component="h2">
-              Text in a modal
-            </Typography>
-            <Typography id="spring-modal-description" sx={{ mt: 2 }}>
-              Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-            </Typography>
-          </Box>
-        </Fade>
-      </Modal>
-    </div>
-  );
-}
-
-const Skill = (props) => {
-    return(
-        <img></img>
-    )
+    return (
+        <div>
+            <Button sx={{width:"150px", margin:"1rem"}} onClick={handleOpen}><img src={img} alt='img'></img></Button>
+            <Modal
+                aria-labelledby="spring-modal-title"
+                aria-describedby="spring-modal-description"
+                open={open}
+                onClose={handleClose}
+                closeAfterTransition
+                slots={{ backdrop: Backdrop }}
+                slotProps={{
+                    backdrop: {
+                        TransitionComponent: Fade,
+                    },
+                }}
+            >
+                <Fade in={open}>
+                    <Box sx={style}>
+                        <Typography id="spring-modal-title" variant="h6" component="h2">
+                            {skill}
+                        </Typography>
+                        <Typography id="spring-modal-description" sx={{ mt: 2 }}>
+                            {description}
+                        </Typography>
+                    </Box>
+                </Fade>
+            </Modal>
+        </div>
+    );
 }
 
 
@@ -108,6 +116,19 @@ const Skills = () => {
         <div className="slider-content-container">
             <div className="centered">
                 <h1>Skills</h1>
+                <div className='skills-container'>
+                    <SpringModal img={reactIcon} skill={"this is a skill"} description={"this is a description"} />
+                    <SpringModal img={expressIcon} skill={"this is a skill"} description={"this is a description"} />
+                    <SpringModal img={nodeIcon} skill={"this is a skill"} description={"this is a description"} />
+                    <SpringModal img={mongoIcon} skill={"this is a skill"} description={"this is a description"} />
+                    <SpringModal img={htmlIcon} skill={"this is a skill"} description={"this is a description"} />
+                    <SpringModal img={cssIcon} skill={"this is a skill"} description={"this is a description"} />
+                    <SpringModal img={jsIcon} skill={"this is a skill"} description={"this is a description"} />
+                    <SpringModal img={cppIcon} skill={"this is a skill"} description={"this is a description"} />
+                    <SpringModal img={pythonIcon} skill={"this is a skill"} description={"this is a description"} />
+                    <SpringModal img={swiftIcon} skill={"this is a skill"} description={"this is a description"} />
+                    <SpringModal img={gitLogo} skill={"this is a skill"} description={"this is a description"} />
+                </div>
             </div>
         </div>
     )
