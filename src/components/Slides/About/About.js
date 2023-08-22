3f9f829578c403ae0fea61a155954e60847e3cdf
Juan Jose Gomez Medina <juanjo.gomez2000@hotmail.com>
2023-08-21 21:15:08 -0400

    Changed text in about

--- src/components/Slides/About/About.js
+++ src/components/Slides/About/About.js
@@ -11,7 +11,7 @@ const About = () => {
                     <p>🔍 Tech is my playground. From iOS to web development, I'm always learning. Currently diving deep into data science at Harvard University.</p>
                     <p>🌟 At Eversana Intouch, I'm an intern crafting web apps with HTML, CSS, React, and more. As a tech lead at the Google Developer Student Club, I ignite minds and foster innovation.</p>
                     <p>🤖 Whether it's AI-infused Pacman games or facial recognition wizardry, I love making machines do magic.</p>
-                    <p>🎓 I'm a computer science enthusiast at USF with a 3.6 GPA. Courses like operating systems, AI/ML, and more keep me curious.</p>
+                    <p>🎓 I'm a Computer Science enthusiast at USF with a 3.4 GPA. Courses like operating systems, AI/ML, and more keep me curious.</p>
                     <p>🌴 When not coding, you'll find me soaking up Tampa's sun. Let's connect and make tech magic! 🌐🤝</p>
                 </div>
             </div>
