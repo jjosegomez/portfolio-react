bd0f515351eba4796ceaee22645181a1c5a5aa6e
Juan Jose Gomez Medina <juanjo.gomez2000@hotmail.com>
2023-10-02 18:27:07 -0400

    fixed skills section

--- src/components/Slides/Skills/Skills.js
+++ src/components/Slides/Skills/Skills.js
@@ -18,6 +18,8 @@ import pythonIcon from "../../../assets/skills/python-icon.svg"
 import cppIcon from "../../../assets/skills/ISO_C++_Logo.svg"
 import swiftIcon from "../../../assets/skills/swift-icon.svg"
 import gitLogo from "../../../assets/skills/git-icon-logo-svgrepo-com.svg"
+import postgresIcon from "../../../assets/skills/postgresql_logo.svg"
+
 
 import "./Skills.css"
 
@@ -63,12 +65,15 @@ Fade.propTypes = {
 };
 
 const style = {
+    textAlign: "center",
     position: 'absolute',
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
     width: 400,
     bgcolor: 'background.paper',
+    bgcolor: 'rgba(30, 30, 30)',
+    color: "white",
     border: '2px solid #000',
     boxShadow: 24,
     p: 4,
@@ -81,7 +86,9 @@ function SpringModal({ skill, img, description }) {
 
     return (
         <div>
-            <Button sx={{ width: "150px", margin: "1rem" }} onClick={handleOpen}><img src={img} alt='img'></img></Button>
+            <Button sx={{ width: "150px", margin: "1rem" }} onClick={handleOpen}>
+                <img src={img} alt='skill-img'></img>
+            </Button>
             <Modal
                 aria-labelledby="spring-modal-title"
                 aria-describedby="spring-modal-description"
@@ -119,64 +126,64 @@ const Skills = () => {
     const skillsArray = [
         {
             skill: "React",
-            img: { reactIcon },
-            description: "this is react description"
+            img: reactIcon,
+            description: "React is a popular JavaScript library for building user interfaces. It allows developers to create reusable UI components and manage the state of an application efficiently. React is widely used for developing single-page applications and interactive user interfaces.",
         },
         {
             skill: "Express",
-            img: { expressIcon },
-            description: "this is express description"
+            img: expressIcon,
+            description: "Express.js is a minimal and flexible Node.js web application framework that provides a robust set of features for web and mobile applications. It is used for building APIs, handling HTTP requests, and serving dynamic content. Express.js simplifies the process of building scalable and maintainable web applications.",
         },
         {
             skill: "Node",
-            img: { nodeIcon },
-            description: "this is node description"
+            img: nodeIcon,
+            description: "Node.js is a JavaScript runtime environment that allows developers to run JavaScript on the server side. It is designed to build scalable and high-performance network applications. Node.js is commonly used for building server-side web applications, APIs, and real-time applications.",
         },
         {
             skill: "MongoDB",
-            img: { mongoIcon },
-            description: "this is mongo description"
+            img: mongoIcon,
+            description: "MongoDB is a popular NoSQL database management system that stores data in flexible, JSON-like documents. It is known for its flexibility, scalability, and ease of use. MongoDB is widely used for building modern, data-driven applications and is particularly suited for handling large volumes of unstructured or semi-structured data.",
         },
         {
             skill: "HTML",
-            img: { htmlIcon },
-            description: "this is html description"
+            img: htmlIcon,
+            description: "HTML (HyperText Markup Language) is the standard markup language used for creating web pages and web applications. It defines the structure and layout of content on the web. HTML elements are represented by tags, which define headings, paragraphs, links, images, and other elements displayed in a web browser.",
         },
         {
             skill: "CSS",
-            img: { cssIcon },
-            description: "this is css description"
+            img: cssIcon,
+            description: "CSS (Cascading Style Sheets) is a stylesheet language used for describing the look and formatting of a document written in HTML. It enables web developers to control the layout, colors, fonts, and spacing of web pages. CSS allows the separation of content from presentation, enhancing the visual appeal and user experience of websites.",
         },
         {
             skill: "JavaScript",
-            img: { jsIcon },
-            description: "this is javascript description"
+            img: jsIcon,
+            description: "JavaScript is a versatile programming language commonly used for web development. It enables interactive and dynamic features on websites, making web pages responsive and engaging. JavaScript can be executed in web browsers and server-side environments, making it a fundamental technology for building modern web applications.",
         },
         {
             skill: "C/C++",
-            img: { cppIcon },
-            description: "this is C/C++ description"
+            img: cppIcon,
+            description: "C and C++ are powerful programming languages known for their high performance and versatility. They are widely used for system programming, game development, embedded systems, and software development that requires optimal performance. C++ is an extension of the C language and includes object-oriented programming features.",
         },
         {
             skill: "Python",
-            img: { pythonIcon },
-            description: "this is python description"
+            img: pythonIcon,
+            description: "Python is a versatile, high-level programming language known for its simplicity and readability. It is widely used for web development, scientific computing, data analysis, artificial intelligence, and automation. Python's extensive libraries and frameworks make it a popular choice for various applications and rapid development.",
         },
         {
             skill: "Swift",
-            img: { swiftIcon },
-            description: "this is swift description"
+            img: swiftIcon,
+            description: "Swift is a powerful and intuitive programming language developed by Apple for iOS, macOS, watchOS, and tvOS app development. It is designed to be fast, safe, and expressive, making it easier for developers to create high-quality, reliable applications for Apple devices. Swift incorporates modern language features and syntax, enhancing developer productivity.",
         },
         {
-            skill: "Posgress",
-            img: { mongoIcon },
-            description: "this is posgress description"
+            skill: "Postgres",
+            img: postgresIcon,
+            description: "PostgreSQL, often referred to as Postgres, is a powerful open-source relational database management system (RDBMS). It provides advanced features such as support for JSON, full-text search, and geospatial data. Postgres is known for its extensibility, standards compliance, and strong community support. It is widely used for various applications that require reliable and scalable database solutions.",
         },
         {
             skill: "Git",
-            img: { gitLogo },
-            description: "this is git description"
-        }
+            img: gitLogo,
+            description: "Git is a distributed version control system used for tracking changes in source code during software development. It allows multiple developers to collaborate on projects efficiently by managing branches, merging code changes, and maintaining a detailed history of revisions. Git is widely adopted in the software development industry for its speed, flexibility, and robust branching capabilities.",
+        }        
     ]
     return (
         <div className="slider-content-container">
