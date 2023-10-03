c760c9f101a1811f71da1f89a57a4f22b4bb9ae3
Juan Jose Gomez Medina <juanjo.gomez2000@hotmail.com>
2023-10-03 11:33:28 -0400

    version 1 of timeline finished

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,72 @@
-//initialize an express js app
-const express = require('express');
-const app = express();
-//initialize a port
-const port = 5000;
-//initialize a path
+class Project {
+    constructor(name, description, language, lastUpdate) {
+        this.name = name;
+        this.description = description;
+        this.language = language;
+        this.lastUpdate = lastUpdate;
+    }
+}
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-})
+// importing cheerio and axios
+const PORT = 5050
+const express = require("express")
+const cheerio = require("cheerio")
+const axios = require("axios")
+
+const app = express();
+
+async function getProjectArray() {
+    // downloading the target web page by performing an HTTP GET request in Axios
+    
+    const axiosResponse = await axios.request({
+        method: "GET",
+        url: "https://github.com/jjosegomez?tab=repositories",
+
+        // By default, Axios will use the following User-Agent: axios <axios_version>
+        // This is not what the User-Agent used by a browser looks like. So, anti-scraping technologies may detect and block your Node.js web scraper.
+        // Set a valid User-Agent header in Axios by adding the following attribute to the object passed to request():
+        headers: {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"
+        }
+    })
+    let projectList = []
+    await axios("https://github.com/jjosegomez?tab=repositories").then((response) => {
+        // parsing the HTML source of the target web page with Cheerio
+        const $ = cheerio.load(response.data)
+
+        //intialize the datastructure containing the information of the product.
+        let count = 0
+        repositoryList = $("#user-repositories-list > ul > li").each((index, element) => {
+            const name = $(element).find("h3 > a").text().trim()
+            const description = $(element).find("div:nth-child(2) > p").text().trim()
+            const language = $(element).find("div.f6.color-fg-muted.mt-2 > span").text().trim()
+            const lastUpdate = $(element).find("div.f6.color-fg-muted.mt-2 > relative-time").text().trim()
+            project = new Project(name, description, language, lastUpdate)
+            projectList.push(project)
+            count++
+        })
+        console.log(count)
+    })
+    console.log(projectList)
+
+    return projectList
+}
+// Note that you can use await only in functions marked with async. 
+// This is why you have to embed your JavaScript web scraping logic in the async performScraping() function.
+
+app.get("/api/projects", async (req, res) => {
+    try {
+      const projects = await getProjectArray();
+      return res.status(200).json({
+        projects: projects,
+      });
+    } catch (err) {
+      return res.status(500).json({
+        err: err.toString(),
+      });
+    }
+  });
+  
+  app.listen(PORT, () =>
+    console.log(`The server is active and running on port ${PORT}`)
+  );
--- src/components/Slides/Timeline/Timeline.js
+++ src/components/Slides/Timeline/Timeline.js
@@ -6,6 +6,7 @@ const ExperienceBlock = (props) => {
         <div className="experience-block">
             <div className="experience-block-header">
                 <h3>{props.company}</h3>
+                <h3> – </h3>
                 <h3>{props.title}</h3>
             </div>
             <div className="description">
@@ -13,6 +14,7 @@ const ExperienceBlock = (props) => {
                 <p>{props.description2}</p>
                 <p>{props.description3}</p>
             </div>
+            <strong style={{textAlign:"center"}}>{props.date}</strong>
         </div>
     )
 }
@@ -20,11 +22,11 @@ const ExperienceBlock = (props) => {
 const Timeline = () => {
     return (
         <div className="slider-content-container">
-            <div className="centered timeline">
-                <ExperienceBlock title="Tech Lead" company="Google Developer Student Club" description1="Worked on the Bloomberg API" description2="Worked on the Bloomberg API" description3="Worked on the Bloomberg API" />
-                <ExperienceBlock title="Software Development Intern" company="Bloomberg" description1="Worked on the Bloomberg API" description2="Worked on the Bloomberg API" description3="Worked on the Bloomberg API" />
-                <ExperienceBlock title="Mentee at Microsoft Mentorship Program " company="Bloomberg" description1="Worked on the Bloomberg API" description2="Worked on the Bloomberg API" description3="Worked on the Bloomberg API" />
-            </div>
+            <div className="timeline">
+            <h1>Timeline</h1>
+            <ExperienceBlock title="Tech Lead" company="Google Developer Student Club" description1="Conducted workshops for USF students, covering web development basics and Google technologies, providing participants with hands-on skills and valuable knowledge to excel in their careers" description2="Collaborated closely with the e-board, playing an integral role in planning and executing exclusive projects that enhanced the club's offerings and fostered greater engagement within the student community" description3="Served as a mentor and guide to students, offering support and encouragement in their programming projects, nurturing their growth, and inspiring creative and innovative solutions" date="January 2023 - Now"/>
+            <ExperienceBlock title="Software Development Intern" company="Eversana Intouch" description1="Led and executed website updates utilizing HTML, CSS, and JavaScript for live client websites at a consulting firm." description2="Pioneered a collaborative intern project with a team of 8 members, serving as the sole developer. Successfully orchestrated the redesign of the website for enhanced modern aesthetics and user-friendliness." description3="Demonstrated strong multitasking abilities by effectively managing and contributing to about 3 simultaneous projects while actively participating in stand-up meetings and regular project reviews with mentor and manager." date="June 2023 - August 2023"/>
+            <ExperienceBlock title="Mentee at Microsoft Mentorship Program" company="Microsoft" description1="Acquired proficiency in Microsoft Technologies, including Power Apps, Power BI, and Power AI, through dedicated learning and practical application." description2="Assisted to workshops on leadership, communication, emotional intelligence, public speaking, teamwork and other topics, supporting enhancing my soft skills and professional development." description3="Collaborated effectively within a small team and actively participated in a Hackathon Project, contributing valuable ideas and technical expertise." date="July 2022 - August 2022" /> </div>
         </div>
     );
 }
