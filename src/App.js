bbc5c7dcccbb1bb26e2eab68a4b52f75830966c7
Juan Jose Gomez Medina <juanjo.gomez2000@hotmail.com>
2023-10-07 12:24:18 -0400

    Started mobile solution

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
-import React from "react";
-import Canvas from "./components/Canvas/Canvas";
-import "./App.css"
+import React, { useState, useEffect } from 'react';
+import MobileView from './components/Mobile/MobileView';
+import Canvas from './components/Canvas/Canvas';
 
 const App = () => {
+    const [mobile, setMobile] = useState(false);
+    const [innerWidth, setInnerWidth] = useState(window.innerWidth);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setInnerWidth(window.innerWidth);
+            if (innerWidth < 850) {
+                setMobile(true);
+            } else {
+                setMobile(false);
+            }
+        };
+
+        // Add event listener for window resize
+        window.addEventListener('resize', handleResize);
+
+        // Clean up the event listener when the component unmounts
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, [innerWidth]); // Empty dependency array ensures the effect runs once after the initial render
+
     return (
         <div className="App">
-            <Canvas/>
+            { !mobile ? <MobileView/>: <Canvas/>}
         </div>
     );
-}
+};
 
 export default App;
