3d774e512d6baa28dbd0c85eda86134eebc9375d
Juan Jose Gomez Medina <juanjo.gomez2000@hotmail.com>
2023-10-13 12:56:56 -0400

    trying to fix bug reactDOM/client

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
-// After
-import { createRoot } from 'react-dom/client';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 
-
-const container = document.getElementById('app');
-const root = createRoot(container); // createRoot(container!) if you use TypeScript
-root.render(<App tab="home" />);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.Fragment>
+    <App />
+  </React.Fragment>
+);
