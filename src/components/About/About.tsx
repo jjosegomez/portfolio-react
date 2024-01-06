fbd28a9b7cacff97a4c8c45abf11d1d9673b5d30
Juan Jose Gomez Medina <juanjo.gomez2000@hotmail.com>
2024-01-06 17:23:28 -0500

    updated about content and changed select styles

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -9,9 +9,9 @@ const About: React.FC = () => {
                     <img src={aboutMeImg}></img>
                 </figure>
                 <div className="about-text">
-                    <h1>👋 Hey, I'm Juan!</h1>
+                    <h1>Hey, I'm Juan!</h1>
                     <br />
-                    <p>I'm Juan José Gomez, a dynamic Computer Science graduate from the University of South Florida dedicated to creating captivating web applications that bring innovative ideas to life. My expertise spans HTML, CSS, JavaScript, React, Node, and more, allowing me to craft seamless user experiences that make a lasting impact.</p>
+                    <p>A dynamic Computer Science graduate from the University of South Florida dedicated to creating captivating web applications that bring innovative ideas to life. My expertise spans HTML, CSS, JavaScript, React, Node, and more, allowing me to craft seamless user experiences that make a lasting impact.</p>
                     <p>🚀 My focus on AI and web development has been instrumental in shaping my journey. I'm currently exploring Data Science courses by Harvard University, continually enriching my skills to stay at the forefront of technological innovation.</p>
                     <p>💼 My experience at Eversana Intouch involved leading projects that revitalized client websites, enhancing user engagement through HTML, CSS, and JavaScript. Additionally, as a tech lead at the Google Developer Student Club, I ignited passion among students and fostered innovation through technical workshops.</p>
                     <p>🌟 I've showcased my skills in AI and web development through personal projects. Collaborating on Prometheus AITutor, a four-person team and I developed an NLP-based Personal Tutor Web Application. My responsibilities ranged from backend (Django) to frontend (React TypeScript) integration, implementing ML models for tailored user learning and testing experiences. Simultaneously, I led the development of the Zillow Clone with AI Realtor Assistant, leveraging the MERN stack to create a dynamic real estate platform. Integrating OpenAI API for a Realtor Assistant feature, employing Passport.js authentication for security, and using Google Places API for location-based services showcased my prowess in AI and web development.</p>
