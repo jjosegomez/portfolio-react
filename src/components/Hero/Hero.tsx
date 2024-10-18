edbea61071ef271cb8069ea7b148b3b98fb32474
Juan Jose Gomez Medina <juanjo.gomez2000@hotmail.com>
2024-10-17 21:32:29 -0500

    updated hero text

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -10,7 +10,7 @@ const Hero = () => {
                     <h2>Software Engineer at Medtronic</h2>
                     <h4>University of South Florida</h4>
                 </div>
-                <span>Innovative thinker and passionate coder ready to tackle the tech world. Specializing in Artificial Intelligence and Web Development. Dedicated to creating meaningful solutions through technology.</span>
+                <span>Versatile thinker and passionate coder ready to tackle complex challenges. Specializing in full-stack development. Dedicated to creating impactful solutions through technology and always learning!</span>
                 <a className="lets-talk-button"  href='https://www.linkedin.com/in/jjgomezswe/' target="_blank">Lets Talk!</a>
             </div>
             <div style={{ backgroundImage: `url(${heroImg})` }} className="hero-img tv"></div>
