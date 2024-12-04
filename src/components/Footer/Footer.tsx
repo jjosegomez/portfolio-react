c4327764581f9ab4610ab905042186807822ec31
Juan Jose Gomez Medina <juanjo.gomez2000@hotmail.com>
2024-12-04 17:59:04 -0500

    Footer update

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -9,7 +9,7 @@ const Footer: React.FC<FooterProps> = () => {
   return (
     <footer>
       <p>Last Updated: <time dateTime='2024-12-04'>Dec 4th, 2024</time></p>
-      <p>&copy; Tech by Juan 2023.</p>
+      <p>&copy; 2023</p>
     </footer>
   );
 };
